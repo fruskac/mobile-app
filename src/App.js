468c320d7ba600ce72f6c70c704956268fee35d4
alex.gvozden <alex.gvozden@gmail.com>
2018-01-25 11:45:22 +0100

    add navigator

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 import MapBox from "@mapbox/react-native-mapbox-gl";
+import { StackNavigator } from "react-navigation";
 
 MapBox.setAccessToken(
   "pk.eyJ1IjoiYWxleGd2b3pkZW4iLCJhIjoiY2pjN2tvM2p1MGV0dzJ3bzcwNzRpNnZ1MyJ9.6vel6zy35B2t9dB3VywO9g"
@@ -19,22 +20,33 @@ const instructions = Platform.select({
     "Shake or press menu button for dev menu"
 });
 
-export default class App extends Component<{}> {
+class App extends Component<{}> {
+  static navigationOptions = {
+    title: "Welcome"
+  };
   render() {
-    return (
-      <View style={styles.container}>
-        <MapBox.MapView
-          zoomLevel={12}
-          minZoom={12}
-          maxZoom={13}
-          styleURL={"mapbox://styles/alexgvozden/cjc7l0w1y3jcr2snwkmzb8vm2"}
-          centerCoordinate={[19.7093, 45.1571]}
-          style={styles.container}
-        />
-      </View>
-    );
+    return <Text>Hello, Navigation!</Text>;
   }
+  // render() {
+
+  //   return (
+  //     <View style={styles.container}>
+  //       <MapBox.MapView
+  //         zoomLevel={12}
+  //         minZoom={12}
+  //         maxZoom={13}
+  //         styleURL={"mapbox://styles/alexgvozden/cjc7l0w1y3jcr2snwkmzb8vm2"}
+  //         centerCoordinate={[19.7093, 45.1571]}
+  //         style={styles.container}
+  //       />
+  //     </View>
+  //   );
+  // }
 }
+
+export default (SimpleApp = StackNavigator({
+  Home: { screen: App }
+}));
 
 const styles = StyleSheet.create({
   container: {
