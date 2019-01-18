ca3accf4c7d0421b417cafbe3f37f39ae67ff708
Administrator <Administrator@Administrators-Mac.local>
2019-01-18 16:06:49 +0100

    iOS build

--- ios/Fruskac/AppDelegate.m
+++ ios/Fruskac/AppDelegate.m
@@ -20,8 +20,11 @@
   
   // jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-  
+  #ifdef DEBUG
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #else
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
 //  for (NSString* family in [UIFont familyNames])
 //  {
 //    NSLog(@"%@", family);
